8447aa1bc035a8f7c3b2f60ff564df9084c7739c
Andrew-Wong-150 <81269030+Andrew-Wong-150@users.noreply.github.com>
2022-02-14 23:48:31 -0500

    Add files via upload

---
+++ transfer_learning.py
@@ -0,0 +1,74 @@
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+
+root = r'C:\Users\Andrew\Documents\AutoDrive\Transfer Learning\root_merged'
+
+dataset = datasets.ImageFolder(
+    root,
+    transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+)
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_loader = torch.utils.data.DataLoader(
+    train_dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=0
+)
+
+test_loader = torch.utils.data.DataLoader(
+    test_dataset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=0
+)
+
+model = models.mobilenet_v3_small(pretrained=True)
+model.classifier[3] = torch.nn.Linear(model.classifier[3].in_features, 4)
+
+device_string = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device(device_string)
+model = model.to(device)
+
+NUM_EPOCHS = 10
+BEST_MODEL_PATH = 'best_model.pth'
+best_accuracy = 0.0
+
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(NUM_EPOCHS):
+    
+    for images, labels in iter(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = F.cross_entropy(outputs, labels)
+        loss.backward()
+        optimizer.step()
+    
+    test_error_count = 0.0
+    for images, labels in iter(test_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        test_error_count += float(torch.sum(torch.abs(labels - outputs.argmax(1))))
+    
+    test_accuracy = 1.0 - float(test_error_count) / float(len(test_dataset))
+    print('%d: %f' % (epoch, test_accuracy))
+    if test_accuracy > best_accuracy:
+        torch.save(model.state_dict(), BEST_MODEL_PATH)
+        best_accuracy = test_accuracy
